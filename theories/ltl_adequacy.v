4ec9817ce0b9599f3e8377624ebc34a7f4649dcc
jihgfee <jihgfee@gmail.com>
2026-07-13 16:02:20 +0200

    Added adequacy for label projection

--- theories/ltl_adequacy.v
+++ theories/ltl_adequacy.v
@@ -231,4 +231,13 @@ Section ltl_adequacy.
     - intros. destruct tr as [[[]|]]; inversion H; simplify_eq; try eauto; try done.
   Qed.
 
+  Lemma ltl_now_label_f_adequate {A} f (x : A) (tr : wf_trace S L Rel) :
+    (↓fl f x)%I tr ≡ (f <$> mjoin (snd <$> head_trace (tr_car tr)) = Some x).
+  Proof.
+    rewrite /ltl_now_label_f. rewrite ltl_now_adequate.
+    split.
+    - intros. destruct tr as [[[]|]]; simpl in *; simplify_eq; try eauto; try done.
+    - intros. destruct tr as [[[]|]]; inversion H; simplify_eq; try eauto; try done.
+  Qed.
+
 End ltl_adequacy.
